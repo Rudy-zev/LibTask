b9ee2d9649375dd9f156e2dca78cd41345eee0ce
rudy_zev <rudygeoffray@hotmail.fr>
2021-11-09 15:23:23 +0100

    Creat project

---
+++ LibTask/LibTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LibTask
+//
+//  Created by Rudy on 09/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
